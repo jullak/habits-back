bd41ff0f5c120753e63737a8271a91c19450afd7
Julia <julia-akzhigitova@yandex.ru>
2020-01-07 02:37:43 +0300

    back to body

--- src/main/java/com/jullak/habits/controller/UserController.java
+++ src/main/java/com/jullak/habits/controller/UserController.java
@@ -5,6 +5,7 @@ import com.jullak.habits.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -15,12 +16,12 @@ public class UserController {
     UserRepository userRepository;
 
     @RequestMapping(value = "/registration", method=RequestMethod.POST, produces = "application/json")
-    public String registrateUser(@RequestParam String nickname, @RequestParam String password) {
-        Optional<User> isExist = userRepository.findByNickname(nickname);
+    public String registrateUser(@RequestBody Map<String, Object> body) {
+        Optional<User> isExist = userRepository.findByNickname((String) body.get("nickname"));
         if (isExist.isPresent()) {
             return "AlreadyExist";
         }
-        userRepository.save(new User(nickname, password));
+        userRepository.save(new User((String) body.get("nickname"), (String) body.get("password")));
         return "I do something)";
     }
 
