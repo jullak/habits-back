5939dc8ace69cd4aeb4c919649f34101669d8c71
Julia <julia-akzhigitova@yandex.ru>
2020-01-07 02:42:36 +0300

    ...

--- src/main/java/com/jullak/habits/controller/UserController.java
+++ src/main/java/com/jullak/habits/controller/UserController.java
@@ -18,9 +18,9 @@ public class UserController {
     @RequestMapping(value = "/registration", method=RequestMethod.POST, produces = "application/json")
     public String registrateUser(@RequestBody Map<String, Object> body) {
         Optional<User> isExist = userRepository.findByNickname((String) body.get("nickname"));
-        if (isExist.isPresent()) {
-            return "AlreadyExist";
-        }
+        //if (isExist.isPresent()) {
+        return "AlreadyExist";
+        //}
         userRepository.save(new User((String) body.get("nickname"), (String) body.get("password")));
         return "I do something)";
     }
