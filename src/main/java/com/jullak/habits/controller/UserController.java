5177ec828ab0ac9389a39cb6e434c6e07586e279
Julia <julia-akzhigitova@yandex.ru>
2020-01-07 01:40:09 +0300

    Body -> param

--- src/main/java/com/jullak/habits/controller/UserController.java
+++ src/main/java/com/jullak/habits/controller/UserController.java
@@ -3,10 +3,7 @@ package com.jullak.habits.controller;
 import com.jullak.habits.model.User;
 import com.jullak.habits.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -18,7 +15,7 @@ public class UserController {
     UserRepository userRepository;
 
     @RequestMapping(value = "/registration", method= RequestMethod.POST)
-    public String registrateUser(@RequestBody String nickname, @RequestBody String password) {
+    public String registrateUser(@RequestParam String nickname, @RequestParam String password) {
         Optional<User> isExist = userRepository.findByNickname(nickname);
         if (isExist.isPresent()) {
             return "AlreadyExist";
